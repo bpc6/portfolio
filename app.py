a0fdd386f141e3d6fb7b9c714787e7959bbb382f
Bryan Covell <bpc6@vt.edu>
2023-10-07 21:39:27 -0400

    formatting and update bio

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open('static/bio.txt', 'r') as bio_file:
+        bio_content = bio_file.read()
+    return render_template('index.html', bio_content=bio_content)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
