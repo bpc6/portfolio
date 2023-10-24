eda8ee9c99617a6ff8845b215c91ff7fe55e8122
Bryan Covell <bpc6@vt.edu>
2023-10-24 18:01:30 -0400

    experience tab and signature home button

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ def home():
         bio_content = bio_file.read()
     return render_template('index.html', bio_content=bio_content)
 
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+# @app.route('/projects')
+# def projects():
+#     return render_template('projects.html')
+#
+# @app.route('/learning')
+# def education():
+#     return render_template('learning.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
